fcadcd5736ba60efdbef7e815d1f74bb8c51e839
joruizdev <joruizd@developercode.dev>
2024-09-21 14:00:31 -0500

    change content message

--- api/mail.js
+++ api/mail.js
@@ -1,7 +1,7 @@
 const RESEND_API_KEY = 're_WWNiDvWM_2na2VywXLFdtpucNp2kfTAKb';
 
-export async function POST(req, res) {
-  const data = req.body
+export async function POST() {
+  const data = Request.body
 
   const response = await fetch('https://api.resend.com/emails', {
     method: 'POST',
@@ -12,13 +12,29 @@ export async function POST(req, res) {
     body: JSON.stringify({
       from: 'Developercode <joruiz@developercode.dev>',
       to: ['joruiz@developercode.dev'],
+      replyTo: `${data.email}`,
       subject: 'CONTACTO WEB DEVELOPERCODE',
+      text: `
+        Nombre: ${data.name}
+        Correo: ${data.email}
+        Mensaje: ${data.message}
+      `,
       html: `
-      <p>${data.name}</p>
-      <p>${data.mail}</p>
-      <p>${data.message}</p>
-    `,
+        <html>
+          <body style="font-family: Arial, sans-serif; color: #333;">
+            <p style="font-size: 16px;"><strong>Nombre:</strong> ${data.name}</p>
+            <p style="font-size: 16px;"><strong>Correo:</strong> ${data.email}</p>
+            <p style="font-size: 16px;"><strong>Mensaje:</strong> ${data.message}</p>
+            <p><strong>Fecha de Envío:</strong> ${new Date().toLocaleString()}</p>
+            <footer style="margin-top: 20px; font-size: 12px; color: #888;">
+              <p>Este correo fue enviado desde Developercode</p>
+              <p><a href="https://www.developercode.dev">https://www.developercode.dev</a></p>
+            </footer>
+          </body>
+        </html>
+      `
     }),
+    
   });
 
   if (response.ok) {
