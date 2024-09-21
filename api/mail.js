7f60910d2b59deef143220251550fb6d6880163a
joruizdev <joruizd@developercode.dev>
2024-09-21 11:38:02 -0500

    add resend

--- api/mail.js
+++ api/mail.js
@@ -1,15 +1,30 @@
+import { Resend } from 'resend';
+const RESEND_API_KEY = import.meta.env.RESEND_API_KEY;
+
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
-      const data = req.body; // Los datos JSON enviados desde el front
-      console.log(data); // Aquí podrías hacer cualquier procesamiento, como enviar un correo
+      const data = req.body
+
+    const resend = new Resend(RESEND_API_KEY);
+
+    await resend.emails.send({
+      from: 'Developercode <joruiz@developercode.dev>',
+      to: ['joruiz@developercode.dev'],
+      subject: 'CONTACTO WEB DEVELOPERCODE',
+      html: `
+        <p>${data.name}</p>
+        <p>${data.mail}</p>
+        <p>${data.message}</p>
+      `,
+    });
 
       // Ejemplo de respuesta exitosa
-      res.status(200).json({ message: "Correo enviado con éxito", data });
+      res.status(200).json({ message: "Correo enviado con éxito", data })
     } catch (error) {
-      res.status(500).json({ error: "Hubo un error al procesar la solicitud" });
+      res.status(500).json({ error: "Hubo un error al procesar la solicitud" })
     }
   } else {
-    res.status(405).json({ message: "Método no permitido" }); // Solo acepta POST
+    res.status(405).json({ message: "Método no permitido" })
   }
 }
