a79b1748bbac10bf2e6f7cbd2546d8e592b6d56d
joruizdev <joruizd@developercode.dev>
2024-09-21 11:19:49 -0500

    remove package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
-  output: "server",
-  adapter: vercel()
+  integrations: [tailwind(), preact()]
 });
