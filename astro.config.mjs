f17504de328bb6ab882f44dd40ef752371d6827d
joruizdev <joruizd@developercode.dev>
2024-09-21 14:41:25 -0500

    remove preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
-import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind()]
 });
